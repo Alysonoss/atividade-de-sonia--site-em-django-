c3d25c64fd41f991af729e785789a1611a462036
Alysonoss <adelsonpost@gmail.com>
2026-03-11 15:39:48 -0300

    Revert "Add files via upload"
    This reverts commit 9f1cce1b8ccfb499e3acabdb758e15f02735870e.

--- cadastro/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('pagina1', views.pagina1, name='views.pagina_interna' ), 
-    path('pagina2', views.pagina2, ), 
-    path('pagina3', views.pagina3, ), 
-    path('pagina4', views.pagina4, ), 
-]
